1ba10a7bf0cccc7cfbf726eec6e9f6338a9f415a
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2020-08-29 21:37:52 +0530

    Update models.py

--- ToDo-RestApi-Vue/todoauth/models.py
+++ ToDo-RestApi-Vue/todoauth/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class Profile(models.Model):             #dummy profile not used 
+    user = models.OneToOneField(User, on_delete=models.CASCADE)   #dummy profile 
 
 
 

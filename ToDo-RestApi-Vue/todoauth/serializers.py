050a3da7dcdb027df58070381d7e81a2c88c1df3
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2020-08-29 21:35:46 +0530

    Add files via upload

--- ToDo-RestApi-Vue/todoauth/serializers.py
+++ ToDo-RestApi-Vue/todoauth/serializers.py
@@ -2,13 +2,19 @@ from .models import *
 from rest_framework import serializers
 from rest_framework import exceptions
 from rest_framework.authtoken.models import Token
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User,auth
 from django.contrib.auth import authenticate
 from django.contrib.auth.hashers import make_password
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
 
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields='__all__'
 
 class UserSerializer(serializers.ModelSerializer):
-    username=models.CharField(max_length=150)
+    username=serializers.CharField(max_length=150)
     password = serializers.CharField(max_length=65, write_only=True)
     email = serializers.EmailField(max_length=255, min_length=4),
     first_name = serializers.CharField(max_length=255)
@@ -43,6 +49,7 @@ class UserSerializer(serializers.ModelSerializer):
                 last_name=self.validated_data['last_name'],
                 ) 
             password=self.validated_data['password']
+            user.is_active = False
             user.set_password(password)
             user.save()
             return user
@@ -58,12 +65,9 @@ class loginSerializer(serializers.Serializer):
         password=data.get("password","")
         
         if username and password:
-            user=authenticate(username=username,password=password)
+            user=auth.authenticate(username=username,password=password)
             if user:
-                if user.is_active:
                     data['user']=user
-                else:
-                    raise exceptions.ValidationError("user is exists")
             else:
                 raise exceptions.ValidationError("user is not valid")
         else:
--- ToDo-RestApi-Vue/todoauth/views.py
+++ ToDo-RestApi-Vue/todoauth/views.py
@@ -8,7 +8,28 @@ from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from rest_framework.views import APIView
 from django.contrib.auth import login,logout
+from django.contrib.auth.models import User,auth
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated,AllowAny,IsAuthenticatedOrReadOnly
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication,BasicAuthentication
 # Create your views here.
+
+#@api_view
+# class retriveTokenView(APIView):
+#     authentication_classes = (TokenAuthentication,)
+#     permission_classes = (IsAuthenticatedOrReadOnly,)
+
+
+@api_view(['GET',])
+@permission_classes((IsAuthenticated, ))    
+def api_get( request):
+    if request.user.is_authenticated:
+        model=User.objects.get(username=request.user.username)
+        serializer=ProfileSerializer(model)
+        return Response(serializer.data)
+    else:
+        return Response("not")
+
 
 class RegisterView(GenericAPIView):
     serializer_class=UserSerializer
@@ -36,10 +57,33 @@ class loginView(APIView):
             return Response({"token":token.key},status=200)
         return Response("Unable to login retry once ")
 
+# class loginView(APIView):
+#     authentication_classes = (BasicAuthentication,SessionAuthentication)
+
+#     def post(self, request, format=None):
+#         data = request.data
+
+#         username = data.get('username', None)
+#         password = data.get('password', None)
+
+#         user = authenticate(username=username, password=password)
+
+#         if user is not None:
+#             if user.is_active:
+#                 login(request, user)
+#                 token,created = Token.objects.get_or_create(user=user)
+#                 return Response({"token":token.key},status=200)
+#             else:
+#                 return Response(status=status.HTTP_404_NOT_FOUND)
+#         else:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+
+
 
 class logoutView(APIView):
 
-    def post(self,request):
+    def get(self,request):
+        request.user.auth_token.delete()
         logout(request)
         return Response("successfully logged out ")
         
